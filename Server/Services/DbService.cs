c64a1e0fba47e7a8185368908241c4fce6db7f12
Artem <leoartemlev@yandex.ru>
2022-07-08 23:40:48 +0500

    Добавил новое сообщение для получения сообщения

--- Server/Services/DbService.cs
+++ Server/Services/DbService.cs
@@ -1,6 +1,10 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.Collections;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using Server.Data;
+using System.Collections.Generic;
 
 namespace Server.Services
 {
@@ -25,6 +29,34 @@ namespace Server.Services
                 Class = "2b",
                 Marks = { { "c++", 20 } }
             });
+        }
+
+        public override Task<UsersArrayReply> GetAllUsers(EmptyUserRequest request, ServerCallContext context)
+        {
+            var db = new DbController("mongodb://localhost:40000");
+            db.Database = request.Database;
+            db.Collection = request.Collection;
+
+            var result = db.GetAllRecords<Person>().Result;
+            var usersList = new List<UserReply>();
+            foreach (var person in result)
+            {
+                var toAdd = new UserReply()
+                {
+                    Name = person.Name,
+                    Age = person.Age,
+                    Class = person.Class
+                };
+                
+                toAdd.Marks.Add(person.Marks);
+                
+                usersList.Add(toAdd);
+            }
+
+            var respones = new UsersArrayReply();
+            respones.Users.Add(usersList);
+
+            return Task.FromResult(respones);
         }
     }
 }
